6c650d4131dbe9dbacc08267e31d6a900c2bef83
Tim Leerhoff <tleerhof@web.de>
2015-08-07 19:18:00 +0200

    Changed test-output

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function (grunt) {
         
         mochacli: {
           options: {
-            reporter: "nyan",
+            reporter: "list",
             ui: "tdd"
           },
           all: ["test/tests.js"]
